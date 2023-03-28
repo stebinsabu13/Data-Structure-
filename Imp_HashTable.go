51de583a78342830de671d780d4e27ca6107e15a
stebinsabu13 <60867706+stebinsabu13@users.noreply.github.com>
2023-03-28 13:50:26 +0530

    Add files via upload
    Implementation of hash table in golang and handling collisions using separate chaining

---
+++ Imp_HashTable.go
@@ -0,0 +1,111 @@
+package main
+
+import "fmt"
+
+const arraySize = 7
+
+type hashTable struct {
+	array [arraySize]Bucket
+}
+type Bucket struct {
+	head *BucketNode
+}
+type BucketNode struct {
+	key  string
+	next *BucketNode
+}
+
+func hash(key string) int {
+	sum := 0
+	for _, v := range key {
+		sum += int(v)
+	}
+	return sum % arraySize
+}
+func (h *hashTable) Insert(key string) {
+	index := hash(key)
+	h.array[index].insert(key)
+}
+func (h *hashTable) Search(key string) bool {
+	index := hash(key)
+	return h.array[index].search(key)
+}
+func (h *hashTable) Delete(key string) {
+	index := hash(key)
+	h.array[index].delete(key)
+}
+func (b *Bucket) insert(key string) {
+	if !b.search(key) {
+		var n BucketNode
+		n.key = key
+		n.next = b.head
+		b.head = &n
+	} else {
+		fmt.Println("Already exsists")
+	}
+}
+func (b *Bucket) search(key string) bool {
+	currentnode := b.head
+	for currentnode != nil {
+		if currentnode.key == key {
+			return true
+		}
+		currentnode = currentnode.next
+	}
+	return false
+}
+func (b *Bucket) delete(key string) {
+	node := b.head
+	if node.key == key {
+		b.head = node.next
+		node.next = nil
+		return
+	}
+	for node.next != nil {
+		if node.next.key == key {
+			node.next = node.next.next
+			return
+		}
+		node = node.next
+	}
+	fmt.Println("Key doesn't exsists")
+}
+func (h hashTable) display() {
+	for v := range h.array {
+		ptr := h.array[v].head
+		if ptr == nil {
+			fmt.Println(ptr)
+			continue
+		}
+		for ptr != nil {
+			fmt.Print(ptr.key, "->")
+			ptr = ptr.next
+		}
+		fmt.Println()
+	}
+}
+func main() {
+	var h hashTable
+	list := []string{
+		"ERIC",
+		"KENNY",
+		"KYLE",
+		"STAN",
+		"BUTTERS",
+		"RANDY",
+	}
+	for _, v := range list {
+		h.Insert(v)
+	}
+	fmt.Println(h.Search("RANDY"))
+	fmt.Println(h.Search("TITUS"))
+	// h.Delete("ERIC")
+	h.Insert("KYLE")
+	h.Insert("JUNMIN")
+	fmt.Println(h.Search("RANDY"))
+	h.display()
+	fmt.Println(h.Search("ERIC"))
+	h.Delete("ERIC")
+	h.display()
+	h.Delete("TITUS")
+}
